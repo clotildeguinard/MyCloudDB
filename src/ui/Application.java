e6367dfa53364d9d5e1325f005bdd50462ca6be9
clogclo <clotilde_guinard@hotmail.fr>
2014-10-17 14:13:41 +0200

    fake ivans class

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -1,5 +1,11 @@
 package ui;
 
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import test.TestLogging;
 import tools.Loggable;
 
 public class Application extends Loggable {
@@ -9,5 +15,20 @@ public class Application extends Loggable {
 	 **/
 	public void main(){
 
+	}
+
+	public static void main(String[] args) throws IOException {
+		openCLI();
+	}
+	private static void openCLI() throws IOException {
+
+		  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		  System.out.print("EchoClient> ");
+	        String s = br.readLine();
+	        try{
+		        System.out.println("got " + s);
+	        }catch(NumberFormatException nfe){
+	            System.err.println("Invalid Format!");
+	        }
 	}
 }
---
+++ src/ui/IvansClass.java
@@ -0,0 +1,17 @@
+package ui;
+
+public class IvansClass {
+	public boolean connect(String ip, int port){
+		return true;
+	}
+	public boolean disconnect(){
+		return true;
+	}
+	public void quit(){
+		// do something
+		System.exit(0);
+	}
+	public boolean send(String message){
+		return true;
+	}
+}
