0e3c85663cba4746e0bd4a047a07906ea1822739
clogclo <clotilde_guinard@hotmail.fr>
2014-10-17 15:34:22 +0200

    add setLevel possibility in loggable abstract class

--- src/tools/Loggable.java
+++ src/tools/Loggable.java
@@ -1,5 +1,7 @@
 package tools;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -17,16 +19,31 @@ import ui.Application;
 public abstract class Loggable {
 	protected static Logger logger;
 	protected final static String logPath = "logs/";
+	protected ConsoleAppender consoleAppender;
 
 	public Loggable() {
 		logger = Logger.getLogger(this.getClass());
 
-		createNewAppender("all");
-		createNewAppender("warn");
-		createNewAppender("fatal");
+		createNewFileAppender("all");
+		createNewFileAppender("warn");
+		createNewFileAppender("fatal");
+		createNewConsoleAppender("all");
 	}
 
-	private static FileAppender createNewAppender(String logLevel) {
+	/**
+	 * 
+	 * @param level
+	 * @return True if succeeded, false if consoleAppender is null
+	 */
+	protected boolean setConsoleLevel(String level) {
+		ConsoleAppender ca = (ConsoleAppender) logger.getAppender("MyConsoleAppender");
+		if (ca != null) {
+			ca.setThreshold(Level.toLevel(level));
+			return true;
+		} return false;
+	}
+
+	private static FileAppender createNewFileAppender(String logLevel) {
 	    RollingFileAppender appender = new RollingFileAppender();
 	    appender.setName("MyFileAppender" + logLevel);
 	    appender.setLayout(new PatternLayout("%d{yyyy-MM-dd HH:mm:ss} %-5p %c{1}:%L - %m%n"));
@@ -34,6 +51,17 @@ public abstract class Loggable {
 	    appender.setAppend(true);
 	    appender.setMaxFileSize("5MB");
 	    appender.setMaxBackupIndex(10);
+	    appender.setThreshold(Level.toLevel(logLevel));
+	    appender.activateOptions();
+
+	    logger.addAppender(appender);
+	    return appender;
+	}
+
+	private static ConsoleAppender createNewConsoleAppender(String logLevel) {
+	    ConsoleAppender appender = new ConsoleAppender();
+	    appender.setName("MyConsoleAppender");
+	    appender.setLayout(new PatternLayout("%d{yyyy-MM-dd HH:mm:ss} %-5p %c{1}:%L - %m%n"));
 	    appender.setThreshold(Level.toLevel(logLevel));
 	    appender.activateOptions();
 
