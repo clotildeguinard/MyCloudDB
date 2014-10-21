57887cc0c6781a70db32392a854cf58d369531a7
NadiaStr <snadia7c0@gmail.com>
2014-10-21 04:43:09 +0200

    Update IvansClass.java

--- src/ui/IvansClass.java
+++ src/ui/IvansClass.java
@@ -1,17 +1,43 @@
 package ui;
 
-public class IvansClass {
-	public boolean connect(String ip, int port){
-		return true;
+/**
+*
+* @author nadiastraton
+*/
+
+public class IvanTest {
+
+	public boolean connect(String ip_adress, int port) {
+		return false;
 	}
-	public boolean disconnect(){
-		return true;
+	
+	public boolean disconnect()
+	{
+		return false;
 	}
-	public void quit(){
-		// do something
-		System.exit(0);
+	
+	public boolean send(String text){
+		return false;
 	}
-	public boolean send(String message){
-		return true;
+	
+	public boolean logLevel(String log_status){
+		return false;
 	}
+	
+	public boolean help(String help_text){
+		return false;
+	}
+	
+	public boolean quit()
+	{
+		return false;
+	}
+	
+	public boolean unknownCommand(String any)
+	{
+		return false;
+		
+	}
+	
 }
+ 
