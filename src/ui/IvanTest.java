878a078102a71302e41e34c0d03f224b4aea1379
NadiaStr <snadia7c0@gmail.com>
2014-10-23 13:02:44 +0200

    sync

---
+++ src/ui/IvanTest.java
@@ -0,0 +1,44 @@
+package ui;
+
+/**
+*
+* @author nadiastraton
+*/
+
+public class IvanTest {
+
+	public boolean connect(String ip_adress, int port) {
+		return false;
+	}
+	
+	public boolean disconnect()
+	{
+		return false;
+	}
+	
+	public boolean send(String text){
+		return false;
+	}
+	
+	public boolean logLevel(String log_status){
+		return false;
+	}
+	
+	public boolean help(String help_text){
+		return false;
+	}
+	
+	public boolean quit()
+	{
+		return false;
+	}
+	
+	public boolean unknownCommand(String any)
+	{
+		return false;
+		
+	}
+	
+}
+ 
+
